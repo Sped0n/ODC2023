030b8eecec248c68e250b64aa374c5bdc99f6e3b
Sped0n <realsped0n@outlook.com>
2023-05-18 19:06:04 +0800

    feat(treasure): refine main func output

--- CV/treasure/__init__.py
+++ CV/treasure/__init__.py
@@ -6,23 +6,30 @@ from .correct import *
 from .dots import *
 
 
-def find_treasure(frame):
+def find_treasure(frame: np.ndarray, debug: bool = False):
     """
-    not final version, debug only
+    find all treasure dots, if debug is True, will return the locating boxes image
+    and the corrected image
     :param frame: image frame (max height: 480px !!!)
-    :return: N/A
+    :param debug: debug mode (will return image results)
+    :return: scaled treasure dots coordinates (in 10x10), locating boxes image (debug only),
+    corrected image (debug only)
     """
     _, blur = img_preprocess(frame)
-    founds = find_locating_boxes(blur)
-    pos = get_locating_coords(founds, 10)
-    cframe, _ = img_correction(frame, pos)
-    cframe = img_rotate(cframe)
-    _, cblur = img_preprocess(cframe)
-    treasure_dots = treasure_identification(cblur)
+    raw_locating_boxes = find_locating_boxes(blur)
+    pos = get_locating_coords(raw_locating_boxes, 10)
+    corrected_frame = ricd(img_correction(frame, pos))
+    _, cf_blur = img_preprocess(corrected_frame)
+    treasure_dots = treasure_identification(cf_blur)
     scaled_dots_coords = []
-    for found in founds:
-        cv2.drawContours(frame, [found], -1, (0, 255, 0), 3)
+    if debug:
+        frame_copy = frame.copy()
+        for found in raw_locating_boxes:
+            cv2.drawContours(frame_copy, [found], -1, (0, 255, 0), 3)
+        for dot in treasure_dots:
+            scaled_dots_coords.append(coord_scale(dot[:-1]))
+            cv2.circle(corrected_frame, dot[:-1], dot[-1], (0, 255, 0), 5)
+        return scaled_dots_coords, frame_copy, corrected_frame
     for dot in treasure_dots:
         scaled_dots_coords.append(coord_scale(dot[:-1]))
-        cv2.circle(cframe, dot[:-1], dot[-1], (0, 255, 0), 5)
-    return scaled_dots_coords, frame, cframe
+    return scaled_dots_coords
