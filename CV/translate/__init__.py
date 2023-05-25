d6f252a4d464e849cf222009d0e6884e67745305
Sped0n <realsped0n@outlook.com>
2023-05-25 19:09:24 +0800

    refactor(translate): move some func to utils

--- CV/translate/__init__.py
+++ CV/translate/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ["coord_to_index", "seq_to_motion"]
+__all__ = ["coord_to_index", "seq_to_motions"]
 
 import numpy as np
+
+from .utils import *
 
 
 def coord_to_index(coords: list[tuple[int, int]]) -> list[tuple[int, int]]:
@@ -18,84 +20,66 @@ def coord_to_index(coords: list[tuple[int, int]]) -> list[tuple[int, int]]:
     return indexes
 
 
-def direction_prediction(
-    curr_coord: tuple[int, int], last_vector: tuple[int, int]
-) -> list[tuple[int, int]]:
-    """
-    get walkable coordinates with current position and last vector
-
-    >>> sorted(direction_prediction((1, 1), (1, 0)))
-    [(1, 0), (1, 2), (2, 1)]
-
-    :param curr_coord: current coordinate
-    :param last_vector: last vector
-    :return: a list of walkable coordinates (max length = 3)
-    """
-    walkable: list = []
-    for neighbor in [
-        (curr_coord[0] - 1, curr_coord[1]),
-        (curr_coord[0] + 1, curr_coord[1]),
-        (curr_coord[0], curr_coord[1] - 1),
-        (curr_coord[0], curr_coord[1] + 1),
-    ]:
-        if neighbor == tuple(np.subtract(curr_coord, last_vector)):
-            continue
-        walkable.append(neighbor)
-    return walkable
-
-
-def seq_to_motion(maze: list[list[int]], seq: list[tuple[int, int]]):
+def seq_to_motions(
+    maze: list[list[int]], seq: list[tuple[int, int]], debug: bool = False
+) -> list[str]:
     """
     convert the coordinate sequence to motion sequence
     :param maze:
     :param seq:
+    :param debug:
     :return:
     """
-    motion = []
-    last_vector = None
+    motions: list[str] = []
+    last_vector: tuple[int, int] | None = None
     for idx, coord in enumerate(seq[:-1]):
-        curr_vector = tuple(np.subtract(seq[idx + 1], coord))
+        motion: str | None = None
+        curr_vector: tuple[int, int] = tuple(np.subtract(seq[idx + 1], coord))
         # skip if last_vector is None
         if last_vector is None:
             last_vector = curr_vector
             continue
-        predictions = direction_prediction(coord, last_vector)
-        walkable_predictions = []
+        predictions: list[tuple, tuple] = direction_prediction(coord, last_vector)
+        walkable_predictions: list[tuple, tuple] = []
         for prediction in predictions:
             if maze[prediction[0]][prediction[1]] == 0:
                 walkable_predictions.append(prediction)
-        follow_last_motion_vector = curr_vector == last_vector
+        follow_last_motion_vector: bool = curr_vector == last_vector
         match len(walkable_predictions):
+            # no walkable predictions, dead end
             case 0:
-                # dead end U-turn scenario
+                # U-turn scenario
                 pass
+            # one walkable prediction, only one way to go
             case 1:
                 # go straight without intersection
                 if follow_last_motion_vector:
                     pass
                 # one-way turn scenario
                 else:
-                    if (
-                        last_vector[0] * curr_vector[1]
-                        - last_vector[1] * curr_vector[0]
-                        > 0
-                    ):
-                        motion.append(f"s_left{coord}{walkable_predictions}")
-                    else:
-                        motion.append(f"s_right{coord}{walkable_predictions}")
+                    match vector_to_direction(last_vector, curr_vector):
+                        case "left":
+                            motion = mod_output_string("left", "single", coord, debug)
+                        case "right":
+                            motion = mod_output_string("right", "single", coord, debug)
+                        # it should not happen, but just in case
+                        case _:
+                            raise ValueError("invalid planning")
             case _:
                 # crossroad scenario
                 if follow_last_motion_vector:
-                    motion.append(f"c_straight{coord}{walkable_predictions}")
+                    motion = mod_output_string("straight", "cross", coord, debug)
                 else:
-                    if (
-                        last_vector[0] * curr_vector[1]
-                        - last_vector[1] * curr_vector[0]
-                        > 0
-                    ):
-                        motion.append(f"c_left{coord}{walkable_predictions}")
-                    else:
-                        motion.append(f"c_right{coord}{walkable_predictions}")
+                    match vector_to_direction(last_vector, curr_vector):
+                        case "left":
+                            motion = mod_output_string("left", "cross", coord, debug)
+                        case "right":
+                            motion = mod_output_string("right", "cross", coord, debug)
+                        # it should not happen, but just in case
+                        case _:
+                            raise ValueError("invalid planning")
         # update last_vector
+        if motion is not None:
+            motions.append(motion)
         last_vector = curr_vector
-    return motion
+    return motions
---
+++ CV/translate/utils.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+def direction_prediction(
+    curr_coord: tuple[int, int], last_vector: tuple[int, int]
+) -> list[tuple[int, int]]:
+    """
+    get walkable coordinates with current position and last vector
+
+    >>> sorted(direction_prediction((1, 1), (1, 0)))
+    [(1, 0), (1, 2), (2, 1)]
+
+    :param curr_coord: current coordinate
+    :param last_vector: last vector
+    :return: a list of walkable coordinates (max length = 3)
+    """
+    walkable: list = []
+    for neighbor in [
+        (curr_coord[0] - 1, curr_coord[1]),
+        (curr_coord[0] + 1, curr_coord[1]),
+        (curr_coord[0], curr_coord[1] - 1),
+        (curr_coord[0], curr_coord[1] + 1),
+    ]:
+        # skip if neighbor is the last coordinate
+        if neighbor == tuple(np.subtract(curr_coord, last_vector)):
+            continue
+        walkable.append(neighbor)
+    return walkable
+
+
+def vector_to_direction(
+    last_vector: tuple[int, int], curr_vector: tuple[int, int]
+) -> str:
+    """
+    get direction from two vectors
+
+    >>> vector_to_direction((1, 0), (0, 1))
+    'left'
+    >>> vector_to_direction((1, 0), (0, -1))
+    'right'
+    >>> vector_to_direction((1, 0), (1, 0))
+    'align'
+    >>> vector_to_direction((0, 1), (0, -1))
+    'align'
+
+    :param last_vector: last vector
+    :param curr_vector: current vector
+    :return: direction
+    """
+    match last_vector[0] * curr_vector[1] - last_vector[1] * curr_vector[0]:
+        case 1:
+            return "left"
+        case -1:
+            return "right"
+        case 0:
+            return "align"
+        case _:
+            raise ValueError(
+                "invalid direction, please check whether the input vectors is an integer vector"
+            )
+
+
+def mod_output_string(
+    content: str,
+    intersection_cond: str,
+    curr_coord: tuple[int, int],
+    debug: bool = False,
+) -> str:
+    """
+    modify the output string
+    :param content:
+    :param intersection_cond:
+    :param curr_coord:
+    :param debug:
+    :return:
+    """
+    if not debug:
+        return content
+    return f"{intersection_cond}_{content}{curr_coord}"
