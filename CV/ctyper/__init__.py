e397616f9fba685b4901492776891371fa352a40
Sped0n <realsped0n@outlook.com>
2023-07-11 20:47:22 +0800

    refactor(ctyper): add custom type aliases and exceptions
    This commit refactors the `ctyper` module by adding custom type aliases for `Coordinate`, `Distance`, `Vector`, `Image`, `Box`, `Array`, `PixelCoordinate`, and `Treasure`. Additionally, it introduces custom exceptions: `NoAnchorFound`, `NullArea`, `QuantityMismatch`, and `TreasureNull`. These changes improve code readability and maintainability.

--- CV/ctyper/__init__.py
+++ CV/ctyper/__init__.py
@@ -1,5 +1,29 @@
 from typing import TypeAlias
+import numpy as np
 
+# custom type aliases
 Coordinate: TypeAlias = tuple[int, int]
 Distance: TypeAlias = int
 Vector: TypeAlias = tuple[int, int]
+Image: TypeAlias = np.ndarray
+Box: TypeAlias = np.ndarray
+Array: TypeAlias = np.ndarray
+PixelCoordinate: TypeAlias = tuple[int, int]
+Treasure: TypeAlias = tuple[int, int, int]
+
+
+# custom exepctions
+class NoAnchorFound(Exception):
+    pass
+
+
+class NullArea(Exception):
+    pass
+
+
+class QuantityMismatch(Exception):
+    pass
+
+
+class TreasureNull(Exception):
+    pass
